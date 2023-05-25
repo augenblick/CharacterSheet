0d18940cb729a546ee5fb6d7659a476a15ad052d
James Grubbs <jngrubbs@uno.edu>
2023-05-25 09:24:39 -0500

    started messing with buffdebuff and godeditmode hotkeys

--- src/views/CharacterSheet.vue
+++ src/views/CharacterSheet.vue
@@ -11,11 +11,35 @@ import { defineComponent } from 'vue';
 export default defineComponent({
       data(){
         return {
-          characterModel : {type: Object, default: null}
+          characterModel : {type: Object, default: null},
+          buffDebuffMode : false,
+          godEditMode: false
         }
       },
       created(){
         this.loadInitialData();
+      },
+      mounted(){
+        document.addEventListener('keydown', (event) =>{
+          console.log(event);
+          if ((event.code === 'ShiftLeft' || event.code === 'ShiftRight') && !(event.code === 'ControlLeft')){
+            this.buffDebuffMode = true;
+          }
+          if ((event.code === 'ShiftLeft' || event.code === 'ShiftRight') && event.code === 'ControlLeft'){
+            this.godEditMode = true;
+          }
+        });
+        document.addEventListener('keyup', (event) =>{
+          console.log(event);
+          if (event.code === 'ShiftLeft' || event.code === 'ShiftRight'){
+            this.buffDebuffMode = false;
+            this.godEditMode = false;
+          }
+          if (event.code === 'ControlLeft'){
+            this.godEditMode = false;
+          }
+
+        });
       },
       methods:{
         loadInitialData(){
@@ -38,8 +62,14 @@ export default defineComponent({
     <h3>'{{ characterModel.characterInfo.characterName }}' the level {{characterModel.level}} {{ characterModel.characterInfo.race }} {{ characterModel.characterInfo.class.className }}, played by {{ characterModel.characterInfo.playerName }}.</h3>
     <br/>
     <button @click="reloadCharacterData">Reload Character</button>
+    <div class="tiny">buffDebuffMode: {{ buffDebuffMode }}</div>
+    <div class="tiny">godEditMode: {{ godEditMode }}</div>
   </main>
 </template>
 
-
+<style scoped>
+.tiny{
+  font-size:x-small;
+}
+</style>
 
